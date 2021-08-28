03693ee4de3acb5d647e974faaa5a6d95bb668a8
batyadvora <89684796+batyadvora@users.noreply.github.com>
2021-08-28 12:27:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
   
-let ktotam = prompt ( 'Кто там?' );
-if (ktotam == null) {
-  alert ( 'Отмена' )
-} else if (!(ktotam == 'админ' && ktotam == null )) {
-  alert ( 'я вас не знаю' )
-} else if ( ktotam == 'админ') {
-let parol = prompt ( 'Пароль?' )
-  if (parol == 'я главный') {
-    alert ( 'Привет!' )
-  } else if (!(parol == 'я главный' && parol == null)) {
-    alert ( 'Неверный пароль' )
-  } else if (parol == null) {
-    alert ( 'отмена' )
-  }
+alert("Я JavaScript!");
